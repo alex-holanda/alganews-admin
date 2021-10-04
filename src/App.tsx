b09fb564964da48c28017eca469af4db84c97b22
Alex Holanda <alex.holanda@outlook.com>
2021-10-04 19:30:29 -0300

    Instalado a biblioteca de gráficos do ant-design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { Col, Row, Typography } from 'antd';
+
+import CompanyMetrics from './app/features/CompanyMetrics';
+
+const { Title, Paragraph } = Typography;
+
 function App() {
   return (
-    <div>
-      <h2>app</h2>
-    </div>
+    <Row>
+      <Col span={24}>
+        <Title level={2}>Olá, José Souza</Title>
+
+        <Paragraph>Este é o resumo da empresa nos últimos 12 meses</Paragraph>
+      </Col>
+      <Col span={24}>
+        <CompanyMetrics />
+      </Col>
+    </Row>
   );
 }
 
