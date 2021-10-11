767730bca699d0bd92b20a10f0dc36ab8072b715
Alex Holanda <alex.holanda@outlook.com>
2021-10-11 10:20:13 -0300

    Identificando erros em abas

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -32,6 +32,19 @@ export default function UserForm() {
   return (
     <Form
       layout={'vertical'}
+      onFinishFailed={(fields) => {
+        const bankAccountErrors = fields.errorFields.reduce(
+          (prev, current) =>
+            current.name.includes('bankAccount') ? prev + 1 : prev,
+          0
+        );
+
+        if (bankAccountErrors > 0) {
+          window.alert(
+            `Existem ${bankAccountErrors} erros na aba dados bancários`
+          );
+        }
+      }}
       onFinish={(form) => {
         console.log(form);
       }}
