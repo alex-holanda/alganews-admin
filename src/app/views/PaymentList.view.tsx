d506a7e654e9d693ca7b363b38244abe47fe4eef
Alex Holanda <alex.holanda@outlook.com>
2021-11-10 15:20:17 -0300

    Paginação e ordenação server side

--- src/app/views/PaymentList.view.tsx
+++ src/app/views/PaymentList.view.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import {
   Button,
-  Popconfirm,
   Row,
   Space,
   Table,
@@ -11,32 +10,36 @@ import {
   Tooltip,
   DatePicker,
   Descriptions,
-  Col,
 } from 'antd';
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import { EyeOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Key, SorterResult } from 'antd/lib/table/interface';
 
 import { Payment } from 'alex-holanda-sdk';
 
 import { usePayments } from '../../core/hooks/usePayments';
-import confirm from 'antd/lib/modal/confirm';
-import { Key } from 'antd/lib/table/interface';
-import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
 import { DoubleConfirm } from '../components/DoubleConfirm';
 
 export default function PaymentListView() {
-  const { payments, fetchPayments } = usePayments();
+  const { payments, fetchPayments, totalElements, fetching } = usePayments();
   const [selectedRowKeys, setSelectedRowKeys] = useState<Key[]>([]);
   const [yearMonth, setYearMonth] = useState<string | undefined>();
+  const [page, setPage] = useState(1);
+  const [sortingOrder, seteSortingOrder] = useState<
+    'asc' | 'desc' | undefined
+  >();
+  const pageSize = 7;
 
   const { xs } = useBreakpoint();
 
   useEffect(() => {
     fetchPayments({
       scheduledToYearMonth: yearMonth,
-      sort: ['scheduledTo', 'desc'],
-      page: 0,
+      sort: ['scheduledTo', sortingOrder || 'desc'],
+      page: page - 1,
+      size: pageSize,
     });
-  }, [fetchPayments, yearMonth]);
+  }, [fetchPayments, yearMonth, page, pageSize, sortingOrder]);
   return (
     <>
       <Row align={'middle'} justify={'space-between'} gutter={24}>
@@ -81,203 +84,85 @@ export default function PaymentListView() {
         </Space>
       </Row>
 
-      <Row>
-        <Col span={24}>
-          <Table<Payment.Summary>
-            dataSource={payments}
-            rowKey={'id'}
-            rowSelection={{
-              selectedRowKeys,
-              onChange: setSelectedRowKeys,
-              getCheckboxProps(payment) {
-                return !payment.canBeApproved ? { disabled: true } : {};
-              },
-            }}
-            columns={[
-              {
-                title: 'Agendamentos',
-                responsive: ['xs'],
-                render(payment: Payment.Summary) {
-                  return (
-                    <Descriptions column={1} size={'small'}>
-                      <Descriptions.Item label={'Editor'}>
-                        {payment.payee.name}
-                      </Descriptions.Item>
+      <Table<Payment.Summary>
+        dataSource={payments}
+        rowKey={'id'}
+        loading={fetching}
+        onChange={(p, f, sorter) => {
+          const { order } = sorter as SorterResult<Payment.Summary>;
+          order === 'ascend'
+            ? seteSortingOrder('asc')
+            : seteSortingOrder('desc');
+        }}
+        rowSelection={{
+          selectedRowKeys,
+          onChange: setSelectedRowKeys,
+          getCheckboxProps(payment) {
+            return !payment.canBeApproved ? { disabled: true } : {};
+          },
+        }}
+        columns={[
+          {
+            title: 'Agendamentos',
+            responsive: ['xs'],
+            render(payment: Payment.Summary) {
+              return (
+                <Descriptions column={1} size={'small'}>
+                  <Descriptions.Item label={'Editor'}>
+                    {payment.payee.name}
+                  </Descriptions.Item>
 
-                      <Descriptions.Item label={'Agendamento'}>
-                        {new Date(payment.scheduledTo).toLocaleDateString(
-                          'pt-BR',
-                          {
-                            day: '2-digit',
-                            month: '2-digit',
-                            year: 'numeric',
-                          }
-                        )}
-                      </Descriptions.Item>
-
-                      <Descriptions.Item label={'Período'}>
-                        {`${new Date(
-                          payment.accountingPeriod.startsOn
-                        ).toLocaleDateString('pt-BR', {
-                          day: '2-digit',
-                          month: '2-digit',
-                          year: 'numeric',
-                        })} - ${new Date(
-                          payment.accountingPeriod.endsOn
-                        ).toLocaleDateString('pt-BR', {
-                          day: '2-digit',
-                          month: '2-digit',
-                          year: 'numeric',
-                        })}`}
-                      </Descriptions.Item>
-
-                      <Descriptions.Item label={'Status'}>
-                        {
-                          <Tag color={payment.approvedAt ? 'blue' : 'warning'}>
-                            {payment.approvedAt
-                              ? `Aprovado em ${new Date(
-                                  payment.approvedAt
-                                ).toLocaleDateString('pt-BR', {
-                                  day: '2-digit',
-                                  month: '2-digit',
-                                  year: 'numeric',
-                                })}`
-                              : 'Aguardando aprovação'}
-                          </Tag>
-                        }
-                      </Descriptions.Item>
-
-                      <Descriptions.Item label={'Ações'}>
-                        <Space>
-                          <Tooltip title={'Detalhar'} placement={'left'}>
-                            <Link to={`${payment.id}`}>
-                              <Button size={'small'} icon={<EyeOutlined />} />
-                            </Link>
-                          </Tooltip>
-
-                          <DoubleConfirm
-                            disabled={!payment.canBeDeleted}
-                            popConfirmTitle={'Remover agendamento?'}
-                            modalTitle={'Remover agendamento'}
-                            modalContent={
-                              'Esta é uma ação irreversível. Ao remover um agendamento, ele não poderá ser recuperado!'
-                            }
-                            onConfirm={() => {
-                              console.log('TODO: remover agendamento');
-                            }}
-                          >
-                            <Tooltip
-                              title={
-                                payment.canBeDeleted ? 'Remover' : 'Já aprovado'
-                              }
-                              placement={'right'}
-                            >
-                              <Button
-                                size={'small'}
-                                icon={<DeleteOutlined />}
-                                disabled={!payment.canBeDeleted}
-                              />
-                            </Tooltip>
-                          </DoubleConfirm>
-                        </Space>
-                      </Descriptions.Item>
-                    </Descriptions>
-                  );
-                },
-              },
-              {
-                dataIndex: 'payee',
-                title: 'Editor',
-                responsive: ['sm'],
-                width: 220,
-                ellipsis: true,
-                render(payee: Payment.Summary['payee']) {
-                  return <Link to={`/usuarios/${payee.id}`}>{payee.name}</Link>;
-                },
-              },
-              {
-                dataIndex: 'scheduledTo',
-                title: 'Agendamento',
-                align: 'center',
-                width: 130,
-                responsive: ['sm'],
-                render(date: string) {
-                  return new Date(date).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                  });
-                },
-              },
-              {
-                dataIndex: 'accountingPeriod',
-                title: 'Período',
-                align: 'center',
-                width: 200,
-                responsive: ['sm'],
-                render(period: Payment.Summary['accountingPeriod']) {
-                  const starts = new Date(period.startsOn).toLocaleDateString(
-                    'pt-BR',
-                    {
+                  <Descriptions.Item label={'Agendamento'}>
+                    {new Date(payment.scheduledTo).toLocaleDateString('pt-BR', {
                       day: '2-digit',
                       month: '2-digit',
                       year: 'numeric',
-                    }
-                  );
+                    })}
+                  </Descriptions.Item>
 
-                  const ends = new Date(period.endsOn).toLocaleDateString(
-                    'pt-BR',
-                    {
+                  <Descriptions.Item label={'Período'}>
+                    {`${new Date(
+                      payment.accountingPeriod.startsOn
+                    ).toLocaleDateString('pt-BR', {
                       day: '2-digit',
                       month: '2-digit',
                       year: 'numeric',
-                    }
-                  );
-                  return `${starts} - ${ends}`;
-                },
-              },
-              {
-                dataIndex: 'approvedAt',
-                title: 'Aprovado',
-                align: 'center',
-                width: 180,
-                responsive: ['sm'],
-                render(approvalDate: Payment.Summary['approvedAt']) {
-                  const approvalDateFormatted = new Date(
-                    approvalDate
-                  ).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                  });
+                    })} - ${new Date(
+                      payment.accountingPeriod.endsOn
+                    ).toLocaleDateString('pt-BR', {
+                      day: '2-digit',
+                      month: '2-digit',
+                      year: 'numeric',
+                    })}`}
+                  </Descriptions.Item>
 
-                  return (
-                    <Tag color={approvalDate ? 'blue' : 'warning'}>
-                      {approvalDate
-                        ? `Aprovado em ${approvalDateFormatted}`
-                        : 'Aguardando aprovação'}
-                    </Tag>
-                  );
-                },
-              },
-              {
-                dataIndex: 'id',
-                title: 'Ações',
-                align: 'center',
-                width: 100,
-                responsive: ['sm'],
-                render(id: number, payment) {
-                  return (
+                  <Descriptions.Item label={'Status'}>
+                    {
+                      <Tag color={payment.approvedAt ? 'blue' : 'warning'}>
+                        {payment.approvedAt
+                          ? `Aprovado em ${new Date(
+                              payment.approvedAt
+                            ).toLocaleDateString('pt-BR', {
+                              day: '2-digit',
+                              month: '2-digit',
+                              year: 'numeric',
+                            })}`
+                          : 'Aguardando aprovação'}
+                      </Tag>
+                    }
+                  </Descriptions.Item>
+
+                  <Descriptions.Item label={'Ações'}>
                     <Space>
                       <Tooltip title={'Detalhar'} placement={'left'}>
-                        <Link to={`${id}`}>
+                        <Link to={`${payment.id}`}>
                           <Button size={'small'} icon={<EyeOutlined />} />
                         </Link>
                       </Tooltip>
 
                       <DoubleConfirm
                         disabled={!payment.canBeDeleted}
-                        popConfirmTitle={'Remover agendamento'}
+                        popConfirmTitle={'Remover agendamento?'}
                         modalTitle={'Remover agendamento'}
                         modalContent={
                           'Esta é uma ação irreversível. Ao remover um agendamento, ele não poderá ser recuperado!'
@@ -300,13 +185,135 @@ export default function PaymentListView() {
                         </Tooltip>
                       </DoubleConfirm>
                     </Space>
-                  );
-                },
-              },
-            ]}
-          />
-        </Col>
-      </Row>
+                  </Descriptions.Item>
+                </Descriptions>
+              );
+            },
+          },
+          {
+            dataIndex: 'payee',
+            title: 'Editor',
+            responsive: ['sm'],
+            width: 220,
+            ellipsis: true,
+            render(payee: Payment.Summary['payee']) {
+              return <Link to={`/usuarios/${payee.id}`}>{payee.name}</Link>;
+            },
+          },
+          {
+            dataIndex: 'scheduledTo',
+            title: 'Agendamento',
+            sorter(a, b) {
+              return 0;
+            },
+            align: 'center',
+            width: 130,
+            responsive: ['sm'],
+            render(date: string) {
+              return new Date(date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              });
+            },
+          },
+          {
+            dataIndex: 'accountingPeriod',
+            title: 'Período',
+            align: 'center',
+            width: 200,
+            responsive: ['sm'],
+            render(period: Payment.Summary['accountingPeriod']) {
+              const starts = new Date(period.startsOn).toLocaleDateString(
+                'pt-BR',
+                {
+                  day: '2-digit',
+                  month: '2-digit',
+                  year: 'numeric',
+                }
+              );
+
+              const ends = new Date(period.endsOn).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              });
+              return `${starts} - ${ends}`;
+            },
+          },
+          {
+            dataIndex: 'approvedAt',
+            title: 'Aprovado',
+            align: 'center',
+            width: 180,
+            responsive: ['sm'],
+            render(approvalDate: Payment.Summary['approvedAt']) {
+              const approvalDateFormatted = new Date(
+                approvalDate
+              ).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              });
+
+              return (
+                <Tag color={approvalDate ? 'blue' : 'warning'}>
+                  {approvalDate
+                    ? `Aprovado em ${approvalDateFormatted}`
+                    : 'Aguardando aprovação'}
+                </Tag>
+              );
+            },
+          },
+          {
+            dataIndex: 'id',
+            title: 'Ações',
+            align: 'center',
+            width: 100,
+            responsive: ['sm'],
+            render(id: number, payment) {
+              return (
+                <Space>
+                  <Tooltip title={'Detalhar'} placement={'left'}>
+                    <Link to={`${id}`}>
+                      <Button size={'small'} icon={<EyeOutlined />} />
+                    </Link>
+                  </Tooltip>
+
+                  <DoubleConfirm
+                    disabled={!payment.canBeDeleted}
+                    popConfirmTitle={'Remover agendamento'}
+                    modalTitle={'Remover agendamento'}
+                    modalContent={
+                      'Esta é uma ação irreversível. Ao remover um agendamento, ele não poderá ser recuperado!'
+                    }
+                    onConfirm={() => {
+                      console.log('TODO: remover agendamento');
+                    }}
+                  >
+                    <Tooltip
+                      title={payment.canBeDeleted ? 'Remover' : 'Já aprovado'}
+                      placement={'right'}
+                    >
+                      <Button
+                        size={'small'}
+                        icon={<DeleteOutlined />}
+                        disabled={!payment.canBeDeleted}
+                      />
+                    </Tooltip>
+                  </DoubleConfirm>
+                </Space>
+              );
+            },
+          },
+        ]}
+        pagination={{
+          current: page,
+          onChange: setPage,
+          total: totalElements,
+          pageSize: pageSize,
+        }}
+      />
     </>
   );
 }
--- src/core/hooks/usePayments.ts
+++ src/core/hooks/usePayments.ts
@@ -1,14 +1,22 @@
-import { Payment, PaymentService } from 'alex-holanda-sdk';
 import { useState, useCallback } from 'react';
+
+import { Payment, PaymentService } from 'alex-holanda-sdk';
+
 export function usePayments() {
   const [payments, setPayments] = useState<Payment.Paginated>();
+  const [fetching, setFetching] = useState(false);
 
   const fetchPayments = useCallback((query: Payment.Query) => {
-    PaymentService.getAllPayments(query).then(setPayments);
+    setFetching(true);
+    PaymentService.getAllPayments(query)
+      .then(setPayments)
+      .finally(() => setFetching(false));
   }, []);
 
   return {
     payments: payments?.content,
+    totalElements: payments?.totalElements,
     fetchPayments,
+    fetching,
   };
 }
