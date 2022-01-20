5498d9d74c1856f2054d0f6f5539eede9b8f168d
Alex Holanda <alex.holanda@outlook.com>
2022-01-20 19:50:57 -0300

    Criado objeto de cadastro

--- src/app/features/EntryForm.tsx
+++ src/app/features/EntryForm.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import {
   Form,
   Row,
@@ -11,31 +13,91 @@ import {
 
 import CurrencyInput from 'app/components/CurrencyInput';
 
+import { CashFlow } from 'alex-holanda-sdk';
+
+import { Moment } from 'moment';
+
+type EntryInputForm = Omit<CashFlow.EntryInput, 'transactedOn'> & {
+  transactedOn: Moment;
+};
+
 function EntryForm() {
+  const [form] = Form.useForm();
+
+  const handleFormSubmit = useCallback(
+    (entry: EntryInputForm) => {
+      console.log(entry);
+      console.log(form);
+    },
+    [form]
+  );
+
   return (
     <>
-      <Form layout={'vertical'}>
+      <Form form={form} layout={'vertical'} onFinish={handleFormSubmit}>
         <Row gutter={16}>
           <Col xs={24}>
-            <Form.Item label={'Descrição'}>
+            <Form.Item
+              label={'Descrição'}
+              name={'description'}
+              rules={[
+                {
+                  required: true,
+                  message: 'Campo obrigatório',
+                },
+              ]}
+            >
               <Input placeholder={'E.g.: Pagamento mensal AWS'} />
             </Form.Item>
           </Col>
 
           <Col xs={24}>
-            <Form.Item label={'Categoria'}>
+            <Form.Item
+              label={'Categoria'}
+              name={['category', 'id']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Campo obrigatório',
+                },
+              ]}
+            >
               <Input placeholder={'E.g.: Pagamento mensal AWS'} />
             </Form.Item>
           </Col>
 
           <Col xs={24} sm={12}>
-            <Form.Item label={'Montante'}>
-              <CurrencyInput onChange={() => null} />
+            <Form.Item
+              label={'Montante'}
+              name={'amount'}
+              rules={[
+                {
+                  required: true,
+                  message: 'Campo obrigatório',
+                },
+              ]}
+            >
+              <CurrencyInput
+                onChange={(_, amount) =>
+                  form.setFieldsValue({
+                    amount,
+                  })
+                }
+              />
             </Form.Item>
           </Col>
 
           <Col xs={24} sm={12}>
-            <Form.Item label={'Data de entrada'}>
+            <Form.Item
+              label={'Data de entrada'}
+              name={'transactedOn'}
+              rules={[
+                {
+                  required: true,
+                  message: 'Campo obrigatório',
+                },
+              ]}
+            >
               <DatePicker format={'DD/MM/YYYY'} style={{ width: '100%' }} />
             </Form.Item>
           </Col>
