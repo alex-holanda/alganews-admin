70ced64c6468dc47b6a79547b08ef128fbbcb243
Alex Holanda <alex.holanda@outlook.com>
2022-02-11 14:14:07 -0300

    Renovando accessToken pelo interceptor

--- src/auth/httpConfig.ts
+++ src/auth/httpConfig.ts
@@ -1,17 +1,59 @@
 import Service from 'alex-holanda-sdk/dist/Service';
+import axios from 'axios';
 import AuthService from './Authorization.service';
 
 Service.setRequestInterceptors(async (request) => {
-  const storage = {
-    accessToken: AuthService.getAccessToken(),
-  };
+  const accessToken = AuthService.getAccessToken();
 
-  const { accessToken } = storage;
-
+  // injeta o token de acesso na requisição
   if (accessToken) {
-    // injeta o token de acesso na requisição
     request.headers['Authorization'] = `Bearer ${accessToken}`;
   }
 
   return request;
 });
+
+Service.setResponseInterceptors(
+  (response) => response,
+  async (error) => {
+    // recupera informações da requisição
+    const originalRequest = error.config;
+
+    // caso o erro seja de autenticação e ainda não foi feito o retry
+    if (error?.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      //recupera o code_verifier e o refresh_token
+      const storage = {
+        codeVerifier: AuthService.getCodeVerifier(),
+        refreshToken: AuthService.getRefreshToken(),
+      };
+
+      const { codeVerifier, refreshToken } = storage;
+
+      // caso algum não exista, não é possível renovar o token
+      if (!refreshToken || !codeVerifier) {
+        window.alert('TODO: implement loguout');
+        return;
+      }
+
+      // renova o token
+      const tokens = await AuthService.getNewToken({
+        refreshToken,
+        codeVerifier,
+      });
+
+      // armazenar o token para novas requisições
+      AuthService.setAccessToken(tokens.access_token);
+      AuthService.setRefreshToken(tokens.refresh_token);
+
+      // implementa o token na requisição
+      originalRequest.headers[
+        'Authorization'
+      ] = `Bearer ${tokens.access_token}`;
+
+      // retorna uma nova chamada do axios com essa requisição
+      return axios(originalRequest);
+    }
+  }
+);
