627651e862665b50e2dba38019e16a7ef5c0f24b
Alex Holanda <alex.holanda@outlook.com>
2022-03-04 11:13:24 -0300

    Logout programático e ver perfil

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -2,8 +2,11 @@ import { Layout, Row, Avatar, Dropdown, Menu, Card, Tag } from 'antd';
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 
 import { useAuth } from 'core/hooks/useAuth';
+import AuthService from 'auth/Authorization.service';
 
 import logo from '../../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import confirm from 'antd/lib/modal/confirm';
 
 const { Header } = Layout;
 
@@ -21,37 +24,57 @@ export default function DefaltLayoutHeader() {
         <Dropdown
           placement={'bottomRight'}
           overlay={
-            <Card style={{ width: 200 }}>
-              <Card bordered={false}>
-                <Card.Meta
-                  title={user?.name}
-                  description={
-                    <Tag
-                      color={
-                        user?.role === 'MANAGER'
-                          ? 'red'
-                          : user?.role === 'EDITOR'
-                          ? 'green'
-                          : 'blue'
-                      }
-                    >
-                      {user?.role === 'EDITOR'
-                        ? 'Editor'
-                        : user?.role === 'MANAGER'
-                        ? 'Gerente'
-                        : 'Assitente'}
-                    </Tag>
-                  }
-                />
-              </Card>
-
-              <Menu>
-                <Menu.Item icon={<UserOutlined />}>Meu perfil</Menu.Item>
-                <Menu.Item icon={<LogoutOutlined />} danger>
-                  Fazer logout
-                </Menu.Item>
-              </Menu>
-            </Card>
+            <Menu style={{ width: 220 }}>
+              {/* <Menu.Item> */}
+                <Card bordered={false}>
+                  <Card.Meta
+                    title={user?.name}
+                    description={
+                      <Tag
+                        color={
+                          user?.role === 'MANAGER'
+                            ? 'red'
+                            : user?.role === 'EDITOR'
+                            ? 'green'
+                            : 'blue'
+                        }
+                      >
+                        {user?.role === 'EDITOR'
+                          ? 'Editor'
+                          : user?.role === 'MANAGER'
+                          ? 'Gerente'
+                          : 'Assitente'}
+                      </Tag>
+                    }
+                  />
+                </Card>
+              {/* </Menu.Item> */}
+              <Menu.Item icon={<UserOutlined />}>
+                <Link to={`/usuarios/${user?.id}`}>Meu perfil</Link>
+              </Menu.Item>
+              <Menu.Item
+                icon={<LogoutOutlined />}
+                danger
+                onClick={() =>
+                  confirm({
+                    title: 'Fazer logout',
+                    content:
+                      'Deseja realmente fazer o logout? Será necessário inserir as credenciais novamente',
+                    onOk() {
+                      AuthService.imperativelySendToLogout();
+                    },
+                    closable: true,
+                    okButtonProps: {
+                      danger: true,
+                    },
+                    okText: 'Fazer logout',
+                    cancelText: 'Permanecer logado',
+                  })
+                }
+              >
+                Fazer logout
+              </Menu.Item>
+            </Menu>
           }
         >
           <Avatar src={user?.avatarUrls.small} alt={user?.name} />
